8ec72f91c27a01beda6a0bcaccd7ca5afe6ab130
ryanhan <ryanjhan10@gmail.com>
2025-02-04 14:44:04 -0800

    made exchange and refresh apis

--- src/api/refresh.js
+++ src/api/refresh.js
@@ -5,6 +5,12 @@ const querystring = require('querystring');
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 
+app.use(cors({
+  origin: 'chrome-extension://mclmkimbojpfimecdcokjfogeeafpnnj', // Allow requests from your Chrome extension
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
